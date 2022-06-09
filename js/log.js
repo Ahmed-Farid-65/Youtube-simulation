69fe29510baee192533b4449553210d57e0993b7
ahmed-farid <ahmed.farid0002@gmail.com>
2022-06-09 17:25:50 +0200

    final commit

---
+++ js/log.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // $("#search-filter-icon").click(function(){
+    //     $(".filter-column").slideToggle("slow");
+    // });
+    $(".remove").click(function(){
+        $(this).parents(".card").hide();
+    });
+});
